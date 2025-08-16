08b16e6293d71d125a877c9e19a9c9bd08400725
TWARIQABDUL <tariqazizibabu@gmail.com>
2025-08-16 17:49:42 +0300

    [Feature] Added login and register endpoint

---
+++ inventory_management/src/main/java/com/inventory/inventory_management/dto/LoginResponse.java
@@ -0,0 +1,13 @@
+package com.inventory.inventory_management.dto;
+
+import lombok.Data;
+
+@Data
+public class LoginResponse {
+    private String message;
+    // private String password;
+
+    public LoginResponse(String message){
+        this.message = message;
+    }
+}
--- inventory_management/src/main/java/com/inventory/inventory_management/repository/UserRepository.java
+++ inventory_management/src/main/java/com/inventory/inventory_management/repository/UserRepository.java
@@ -1,7 +1,11 @@
 package com.inventory.inventory_management.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.inventory.inventory_management.entities.User;
 
-public interface UserRepository extends JpaRepository <User,Integer> {}
+public interface UserRepository extends JpaRepository <User,Integer> {
+    Optional<User> findByEmail(String email);
+}
